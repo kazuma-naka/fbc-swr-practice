36044ec84b6854bb906d734000e6592d77cd7fea
kazuma-naka <kazuma.naka.01@gmail.com>
2024-10-19 10:16:24 -0400

    install lottie for loading, success and error animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from "react";
 import useSWR from "swr";
+import Lottie from "react-lottie";
 import "./App.css";
+import loadingAnimation from "./loading.json";
+import successAnimation from "./success.json";
+import errorAnimation from "./error.json";
 
 const fetcher = (url) =>
   fetch(url, { headers: { Accept: "application/json" } }).then((res) => {
     if (!res.ok) {
-      throw new Error(`HTTP Error Status: ${res.status}`);
+      throw new Error(`HTTP error! Status: ${res.status}`);
     }
     return res.json();
   });
@@ -12,9 +17,50 @@ const fetcher = (url) =>
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const { data, error } = useSWR(url, fetcher);
-  if (!data && !error) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-  return <div>{data && <p>Status: {data.description}</p>}</div>;
+  const loadingAnimationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const successAnimationOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: successAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const errorAnimationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: errorAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="container">
+      {!data && !error && (
+        <Lottie options={loadingAnimationOptions} height={200} width={200} />
+      )}
+      {error && (
+        <div className="error-parent">
+          <Lottie options={errorAnimationOptions} height={200} width={200} />
+          <p>Error: {error.message}</p>
+        </div>
+      )}
+      {data && (
+        <div className="success-parent">
+          <Lottie options={successAnimationOptions} height={200} width={200} />
+          <p>Status: {data.description}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
