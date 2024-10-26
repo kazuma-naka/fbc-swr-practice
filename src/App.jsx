c425062299e71486eda211da8e7c08e0e0471ce7
kazuma-naka <kazuma.naka.01@gmail.com>
2024-10-26 08:51:05 -0400

    animationOption の重複を取り除くため引数によってanimationOption を返す関数を作成する

--- src/App.jsx
+++ src/App.jsx
@@ -14,48 +14,59 @@ const fetcher = (url) =>
     return res.json();
   });
 
-function App() {
-  const url = "https://httpstat.us/200?sleep=2000";
-  const { data, error } = useSWR(url, fetcher);
-  const loadingAnimationOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: loadingAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const successAnimationOptions = {
-    loop: false,
-    autoplay: true,
-    animationData: successAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const errorAnimationOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: errorAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+const animationOption = (type) => {
+  if (type === "load") {
+    return {
+      loop: true,
+      autoplay: true,
+      animationData: loadingAnimation,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+  } else if (type === "success") {
+    return {
+      loop: false,
+      autoplay: true,
+      animationData: successAnimation,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+  } else if (type === "error") {
+    return {
+      loop: true,
+      autoplay: true,
+      animationData: errorAnimation,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+  }
+  return null;
+};
 
+function App() {
+  const url = "https://httpstat.us/200?sleep=2000a";
+  const { data, error } = useSWR(url, fetcher);
   return (
     <div className="container">
       {!data && !error && (
-        <Lottie options={loadingAnimationOptions} height={200} width={200} />
+        <Lottie options={animationOption("load")} height={200} width={200} />
       )}
       {error && (
         <div className="error-parent">
-          <Lottie options={errorAnimationOptions} height={200} width={200} />
+          <Lottie options={animationOption("error")} height={200} width={200} />
           <p>Error: {error.message}</p>
         </div>
       )}
       {data && (
         <div className="success-parent">
-          <Lottie options={successAnimationOptions} height={200} width={200} />
+          <Lottie
+            options={animationOption("success")}
+            height={200}
+            width={200}
+          />
           <p>Status: {data.description}</p>
         </div>
       )}
